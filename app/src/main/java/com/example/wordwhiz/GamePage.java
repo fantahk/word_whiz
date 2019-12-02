16c4f7618094553780dc395721ca58c2845f68fd
fantah_k <fkabba2@illinois.edu>
2019-12-02 02:20:27 -0600

    Working on game page (hopefully almost done), xml files updated

--- app/src/main/java/com/example/wordwhiz/GamePage.java
+++ app/src/main/java/com/example/wordwhiz/GamePage.java
@@ -1,25 +1,27 @@
 package com.example.wordwhiz;
 
-import android.content.Context;
+// import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.widget.Button;
 import java.util.Random;
-import android.widget.RelativeLayout;
+// import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class GamePage {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GamePage extends AppCompatActivity {
 
     /**
      * random - going to be used throughout for words and definitions.
      */
     private Random random = new Random();
-    /**
-     * gameLayout - the layout of the game page.
-     */
-    private RelativeLayout gameLayout;
+
     /**
      * currentScore- TextView that displays score
      */
-    private TextView currentScore = gameLayout.findViewById(R.id.currentScore);
+    private TextView currentScore = findViewById(R.id.currentScore);
+
     /**
      * score- keeps track of score
      */
@@ -28,10 +30,10 @@ public class GamePage {
     /**
      * answers - array of all four buttons, will randomly assign correct one.
      */
-    private Button[] answers = {gameLayout.findViewById(R.id.answer0),
-            gameLayout.findViewById(R.id.answer1),
-            gameLayout.findViewById(R.id.answer2),
-            gameLayout.findViewById(R.id.answer3)};
+    private Button[] answers = {findViewById(R.id.answer0),
+            findViewById(R.id.answer1),
+            findViewById(R.id.answer2),
+            findViewById(R.id.answer3)};
 
     /**
      * randomInt- used for setting random answer.
@@ -39,24 +41,49 @@ public class GamePage {
     private int randomInt = random.nextInt(4);
 
     /**
+     * correctAnswer - the right answer. randomly chosen.
+     */
+    private Button correctAnswer = answers[randomInt];
+
+    /**
      * definition - display of definition
      */
-    private TextView definition = gameLayout.findViewById(R.id.definition);
+    private TextView definition = findViewById(R.id.definition);
 
-    public void newRound() {
-        Button correctAnswer = answers[randomInt];
-        // random word variable = API request for random word
-        correctAnswer.setText("Correct Word"); // Will turn into random word variable.
-        // set definition to API definition for random word variable
-    }
-    public void correctAnswerClicked() {
-        score++;
-        currentScore.setText("Score: " + score);
-       // definition.setText("Definition" + score); // Change to API stuff,, checking to make sure definition changes.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_page);
+        // actual code starting now
+
+        // not sure if 59-63 is right,, trying to figure out how to initialize game page for the first round
+        currentScore.setText("Score: 0");
         answers[0].setText("New Word1" + score); // API to get random word
         answers[1].setText("New Word2" + score); // API to get random word
         answers[2].setText("New Word3" + score); // API to get random word
         answers[3].setText("New Word4" + score); // API to get random word
+        newRound();
+        // when right answer is chosen, call correctAnswerClicked()
+        correctAnswer.setOnClickListener(unused -> correctAnswerClicked());
+    }
+
+    public void newRound() {
+        // random word variable = API request for random word
+        correctAnswer.setText("Correct Word"); // sets text of correct button to random word variable
+        definition.setText("Definition: " + score);// set definition to API definition for random word variable
+    }
+    public void correctAnswerClicked() {
+        // change button color to green
+        correctAnswer.setBackgroundColor(Color.GREEN);
+        // update score
+        score++;
+        currentScore.setText("Score: " + score);
+        // go through answers array, only set text of answers that are wrong, set text to random word (moving to new round by setting new words)
+        for (Button answer : answers) {
+            if (answer != correctAnswer) {
+                answer.setText("New Word" + score);
+            }
+        }
         newRound();
     }
 }

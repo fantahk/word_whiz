a17de03d693045f3167b9454d7cdde30b272476e
madis <mbeneke3@illinois.edu>
2019-12-02 16:29:42 -0600

    Merge branch 'master' of https://github.com/fantahk/word_whiz
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/example/wordwhiz/GamePage.java
    #	app/src/main/java/com/example/wordwhiz/HomePage.java
    #	app/src/main/java/com/example/wordwhiz/LoserPage.java
    #	app/src/main/java/com/example/wordwhiz/MainActivity.java
    #	app/src/main/res/layout/game_page.xml
    #	app/src/main/res/layout/home_page.xml
    #	app/src/main/res/layout/loser_page.xml

--- app/src/main/java/com/example/wordwhiz/HomePage.java
+++ app/src/main/java/com/example/wordwhiz/HomePage.java
@@ -1,4 +1,30 @@
 package com.example.wordwhiz;
 
-public class HomePage {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * HomePage is the page that first launches when the app is run. At the top of the screen, the rules are
+ * displayed in a text box. The high score of the player is displayed below that. At the center is
+ * a start button that launches GamePage. At the bottom of the screen is the word of the day, which
+ * will be a random word and definition that is randomized each time the home screen is visited.
+ */
+public class HomePage extends AppCompatActivity {
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+        Button start = findViewById(R.id.start);
+        start.setOnClickListener(unused -> goToGame());
+    }
+    public void goToGame() {
+        Intent intent = new Intent(this, GamePage.class);
+    }
 }

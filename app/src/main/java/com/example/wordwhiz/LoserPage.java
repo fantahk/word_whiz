ab86552da7d700103fa9ad58193f545c72359f4a
tanvirj2 <tanvirj2@illinois.edu>
2019-12-02 16:37:21 -0600

    Loser page UI

--- app/src/main/java/com/example/wordwhiz/LoserPage.java
+++ app/src/main/java/com/example/wordwhiz/LoserPage.java
@@ -1,17 +1,27 @@
 package com.example.wordwhiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class LoserPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loser_page);
+        Button tryAgain = findViewById(R.id.tryAgain);
+        Button home = findViewById(R.id.home);
+        tryAgain.setOnClickListener(unused -> goToGame());
+        home.setOnClickListener(unused -> goToHome());
+    }
+    public void goToGame() {
+        Intent intent = new Intent(this, GamePage.class);
+
+    }
+    public void goToHome() {
+        Intent intent = new Intent(this, HomePage.class);
     }
 }

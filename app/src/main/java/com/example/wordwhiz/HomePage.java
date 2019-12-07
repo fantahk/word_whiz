7a193c9330365e478e5d3e49274620e67d98086f
madis <mbeneke3@illinois.edu>
2019-12-07 10:44:16 -0600

    12/7/19 10:44 AM

--- app/src/main/java/com/example/wordwhiz/HomePage.java
+++ app/src/main/java/com/example/wordwhiz/HomePage.java
@@ -12,10 +12,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -29,12 +32,13 @@ public class HomePage extends AppCompatActivity {
      *
      * @param savedInstanceState
      */
+    private String word;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
         TextView wordOfDay = findViewById(R.id.wordDefinition);
-
+        /*
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=7&maxLength=14&api_key=1zm37ehk7ihwitkbl0id0hxydy2s5l9pamrav08k0bji5wjew";
@@ -61,6 +65,33 @@ public class HomePage extends AppCompatActivity {
         //MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
         Button start = findViewById(R.id.start);
         start.setOnClickListener(unused -> goToGame());
+
+         */
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=1zm37ehk7ihwitkbl0id0hxydy2s5l9pamrav08k0bji5wjew";
+        //JSONObject randomWord = Request.Method.GET;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            System.out.println(response.length());
+                            JSONObject jsonObject = response.getJSONObject(0);
+                            //System.out.println(jsonObject);
+                            word = jsonObject.getString("word");
+                            //System.out.println(word);
+                            // System.out.println(randomWords);
+                        } catch (JSONException e) {
+                            System.out.println("rip");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        });
+        queue.add(jsonArrayRequest);
     }
     public void goToGame() {
         Intent intent = new Intent(this, GamePage.class);

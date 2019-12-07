92860593ff508ee5b5c5f0720ad54fa2f1043136
madis <mbeneke3@illinois.edu>
2019-12-07 10:50:42 -0600

    12/7/19 10:50 AM

--- app/src/main/java/com/example/wordwhiz/HomePage.java
+++ app/src/main/java/com/example/wordwhiz/HomePage.java
@@ -2,7 +2,6 @@ package com.example.wordwhiz;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,8 +12,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -38,38 +35,11 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
         TextView wordOfDay = findViewById(R.id.wordDefinition);
-        /*
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=7&maxLength=14&api_key=1zm37ehk7ihwitkbl0id0hxydy2s5l9pamrav08k0bji5wjew";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        wordOfDay.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                wordOfDay.setText(error.getMessage());
-            }
-        });
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-
-        // Access the RequestQueue through your singleton class.
-        //MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
         Button start = findViewById(R.id.start);
         start.setOnClickListener(unused -> goToGame());
 
-         */
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=1zm37ehk7ihwitkbl0id0hxydy2s5l9pamrav08k0bji5wjew";
-        //JSONObject randomWord = Request.Method.GET;
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONArray>() {
                     @Override
@@ -77,12 +47,10 @@ public class HomePage extends AppCompatActivity {
                         try {
                             System.out.println(response.length());
                             JSONObject jsonObject = response.getJSONObject(0);
-                            //System.out.println(jsonObject);
                             word = jsonObject.getString("word");
-                            //System.out.println(word);
-                            // System.out.println(randomWords);
+                            wordOfDay.setText(word);
                         } catch (JSONException e) {
-                            System.out.println("rip");
+                            wordOfDay.setText(e.getMessage());
                         }
                     }
                 }, new Response.ErrorListener() {

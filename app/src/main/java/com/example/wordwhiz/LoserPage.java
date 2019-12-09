37a56511e877213fd3ba4c4472f14daab6b580fc
tanvirj2 <tanvirj2@illinois.edu>
2019-12-09 11:02:58 -0600

    added java docs to losers page and some variable edits.

--- app/src/main/java/com/example/wordwhiz/LoserPage.java
+++ app/src/main/java/com/example/wordwhiz/LoserPage.java
@@ -9,18 +9,37 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+/**
+ * This is the Loser Page. The Loser Page is where the user goes once he presses the wrong button. The Loser Page has two buttons try again and home. Try again takes you back to the game page to start another round. The Home button takes you to the home page.
+ */
 public class LoserPage extends AppCompatActivity {
+
+    private RelativeLayout homeLayout;
+    /**
+     * Loser text used to display different texts based on the score of the user.
+     */
     private TextView loserText;
+    /**
+     * game score - is the users latest score.
+     */
+    private TextView gameScore;
+    /**
+     * home - takes user back to home page.
+     */
+    private Button home;
+    /**
+     * try again - takes you back to game page for another round.
+     */
+    private Button tryAgain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loser_page);
-        Button tryAgain = findViewById(R.id.tryAgain);
-        Button home = findViewById(R.id.home);
-        TextView gameScore = findViewById(R.id.gameScore);
+        tryAgain = findViewById(R.id.tryAgain);
+        home = findViewById(R.id.home);
+        gameScore = findViewById(R.id.gameScore);
         loserText = findViewById(R.id.loserText);
-        RelativeLayout homeLayout = findViewById(R.id.homeLayout);
+        homeLayout = findViewById(R.id.homeLayout);
         gameScore.setText("Final Score : " + GamePage.score);
         GradientDrawable gradient = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM,
                 new int[] {0xFFd93030, 0xFF962323});
@@ -38,11 +57,19 @@ public class LoserPage extends AppCompatActivity {
         tryAgain.setOnClickListener(unused -> goToGame());
         home.setOnClickListener(unused -> goToHome());
     }
+
+    /** this function takes you to the game page once try again button is clicked above.
+     *
+     */
     public void goToGame() {
         Intent intent = new Intent(this, GamePage.class);
         startActivity(intent);
 
     }
+
+    /** this function takes you to the home page once the home button is clicked above.
+     *
+     */
     public void goToHome() {
         Intent intent = new Intent(this, HomePage.class);
         startActivity(intent);
